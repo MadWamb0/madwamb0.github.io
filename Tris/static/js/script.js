e914918c381f72a5bc4c5b7950e662efbde41063
MadWamb0 <mpreium@gmail.com>
2023-03-18 15:10:42 +0100

    inziato BOT level1

--- Tris/static/js/script.js
+++ Tris/static/js/script.js
@@ -9,6 +9,8 @@ let count = 0
 let winnerBox = document.getElementById("winnerBox")
 let winner = document.getElementById("winner")
 
+let j=0
+
 
 document.querySelectorAll("th").forEach(ciao=>ciao.addEventListener("click", (e)=>{
 	//console.log(e.currentTarget.getAttribute("cell"),e.currentTarget.parentNode.getAttribute("row"))
@@ -94,7 +96,41 @@ function victory(cells){
 
 
 
+function getEmptyCells(array){
+	let emptyCells = []
+	for (var i = 0; i < array.length; i++) {
+		for (var j = 0; j < array[i].length; j++) {
+			if(array[i][j]===null){
+				let mossa = [i,j]
+				emptyCells.push(mossa)
+			}
+		}
+	}
+	return emptyCells
+}
 
+function minimax(){
+	return 1
+}
+
+function bestScore(array, player){
+	let bestScore
+
+	if(!player){
+		// player = x = min
+		bestScore= +Infinity
+
+	}else{
+		// player = O = max
+		bestScore= -Infinity
+	}
+
+
+	for (var k = 0 ; k < emptyCells.length; k++) {
+		 let currentMove = emptyCells[k]
+		 
+	}
+}
 
 
 
